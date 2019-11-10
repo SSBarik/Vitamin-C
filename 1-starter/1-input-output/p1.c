32d80dfac4bf2a8137a800fc4759aa54a5513508
Shashank Shekhar Barik <35847062+SSBarik@users.noreply.github.com>
2019-11-10 08:20:56 +0530

    scanf using assignment supression

--- 1-starter/1-input-output/p1.c
+++ 1-starter/1-input-output/p1.c
@@ -1,5 +1,5 @@
 /*
-WAP to get date as the user input and display it as per the expected output.
+WAP to accept date(format: date/month/year or date-month-year) as the user input and display the date, mohtn & year separately.
 
 Expected Output:
 --------------------------------
@@ -14,9 +14,22 @@ Year: 2019
 int main(void)
 {
 	int date, month, year;
+	
+	printf("Input date: ");
+	scanf("%d%*[/-]%d%*[/-]%d%[/-]", &date, &month, &year); //using assignment suppression, less memory usage as there are still 5 format specifiers but only 3 arguments
+	printf("Date: %d \nMonth: %d \nYear: %d", date, month, year);
+	
+	return 0;
+}
+
+/*
+int main(void)
+{
+	int date, month, year;
 	char separator[2];
 	printf("Input date: ");
 	scanf("%d%[/-]%d%[/-]%d%[/-]", &date, separator, &month, separator, &year); //using search sets, more memory usage due to the separator string
 	printf("Date: %d \nMonth: %d \nYear: %d", date, month, year);
 	return 0;
 }
+*/
