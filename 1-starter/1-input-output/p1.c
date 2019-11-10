d94089799b66852af7f333bbf7b177b64d40028e
Shashank Shekhar Barik <35847062+SSBarik@users.noreply.github.com>
2019-11-10 08:15:20 +0530

    Update p1.c

--- 1-starter/1-input-output/p1.c
+++ 1-starter/1-input-output/p1.c
@@ -11,7 +11,12 @@ Year: 2019
 
 #include<stdio.h>
 
-int main()
+int main(void)
 {
+	int date, month, year;
+	char separator[2];
+	printf("Input date: ");
+	scanf("%d%[/-]%d%[/-]%d%[/-]", &date, separator, &month, separator, &year); //using search sets, more memory usage due to the separator string
+	printf("Date: %d \nMonth: %d \nYear: %d", date, month, year);
 	return 0;
 }
