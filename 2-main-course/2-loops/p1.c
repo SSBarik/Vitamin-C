a4990410cd8eb3773812539cf0f4f6b965ee0dec
Shashank Shekhar Barik <35847062+SSBarik@users.noreply.github.com>
2020-01-31 22:18:39 +0530

    Update p1.c

--- 2-main-course/2-loops/p1.c
+++ 2-main-course/2-loops/p1.c
@@ -14,5 +14,14 @@ Expected Output
 
 int main()
 {
+	int i, j;
+	for(i = 0; i < 5; i++)   // to continue for 5 levels
+	{
+		for(j = 5; j > i; j--)  // to print the spaces
+			printf(" ");
+		for(j = 0; j < i*2+1; j++) // to print the '*'	
+			printf("*");
+		printf("\n");	// for newline	
+	}
 	return 0;
 }
