95f857bdf0d663b46f9b53359db7bba73d2ad3ae
Lomitaswa Suna <lomitaswa@users.noreply.github.com>
2020-10-01 22:51:09 +0530

    Update p1.c

--- 1-starter/2-operators-expressions/p1.c
+++ 1-starter/2-operators-expressions/p1.c
@@ -11,5 +11,17 @@ Reverse of the number is 654.
 
 int main()
 {
+    int num, rev = 0;
+    printf("Enter a three digit number : ");
+    scanf("%d", &num);
+    
+    while (num != 0)
+    {
+        rev = rev*10;
+        rev = rev+num%10;
+        num = num/10;
+    }
+    
+    printf("Reverse of the number : %d\n", rev);
 	return 0;
 }
