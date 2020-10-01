52bc4dd5320248d9d91c54c64d930f4b3b4c60df
Lomitaswa Suna <lomitaswa@users.noreply.github.com>
2020-10-01 22:55:52 +0530

    Update p1.c

--- 1-starter/2-operators-expressions/p1.c
+++ 1-starter/2-operators-expressions/p1.c
@@ -5,10 +5,9 @@ Expected Output:
 --------------------------
 Enter a 3-digit number: 456
 Reverse of the number is 654.
-/*
+*/
 
 #include<stdio.h>
-
 int main()
 {
     int num, rev = 0;
@@ -23,5 +22,5 @@ int main()
     }
     
     printf("Reverse of the number : %d\n", rev);
-	return 0;
+    return 0;
 }
