a42f181e6a3f9cc91384588de77a82509543609a
Tushar Agrawal <96322907+Tushar7890@users.noreply.github.com>
2022-10-01 18:57:41 +0530

    Updated p4.c

--- 2-main-course/2-loops/p4.c
+++ 2-main-course/2-loops/p4.c
@@ -1,6 +1,5 @@
 /*
 P3. Print the following.
-
 Expected Output
 ---------------------------------
 A
@@ -12,5 +11,14 @@ D D D D
 
 int main()
 {
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            char c = 'A' + i;
+            printf("%c ", c);
+        }
+        printf("\n");
+    }
 	return 0;
 }
