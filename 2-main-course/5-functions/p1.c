75fe49dfb1a5a6a5e6b9521a4920cf22700cdc14
Shashank Shekhar Barik <35847062+SSBarik@users.noreply.github.com>
2022-10-02 16:31:20 +0530

    Update p1.c

--- 2-main-course/5-functions/p1.c
+++ 2-main-course/5-functions/p1.c
@@ -3,15 +3,33 @@ P1. WAP to print fibonacci series upto n term using recursion.
 
 Expected Output
 -----------------------------
-Enter number of term: 7
+Enter number of terms: 7
 Fibonacci Series:
 0, 1, 1, 2, 3 , 5, 8
 
 */
 
 #include<stdio.h>
+   
+void printFibonacci(int n)
+{    
+    static int n1 = 0, n2 = 1, n3;    
+    if(n > 0) {    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         printf("%d ",n3);    
+         printFibonacci(n-1);    
+    }    
+}
 
-int main()
-{
-	return 0;
+int main() 
+{    
+    int n;    
+    printf("Enter the number of terms: ");    
+    scanf("%d", &n);    
+    printf("Fibonacci Series: ");    
+    printf("%d %d ", 0, 1);    
+    printFibonacci(n-2);  
+  return 0;  
 }
