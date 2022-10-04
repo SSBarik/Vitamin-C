83113083b4b9abd6c0ec0413456b97ef8ebbed42
lomitaswa <lomitaswa.suna@hotmail.com>
2022-10-04 12:37:19 +0530

    Added solution for 2-main-course/4-strings/p2.c

--- 2-main-course/4-strings/p2.c
+++ 2-main-course/4-strings/p2.c
@@ -10,8 +10,24 @@ Your name in reverse: KNAHSAHS
 */
 
 #include<stdio.h>
+#include<string.h>
+#define MAX_LIMIT 20
 
 int main()
 {
+    char name[MAX_LIMIT], revName[MAX_LIMIT];
+    int i, j, nameLength;
+
+    printf("Enter your name: ");
+    fgets(name, MAX_LIMIT, stdin);
+
+    nameLength = strlen(name);
+
+    // reverse the string
+    for(j = nameLength-1 ,i = 0; i < nameLength; i++, j--)
+        revName[i] = name[j-1];
+
+    printf("Your name in reverse: %s\n", revName);
+
 	return 0;
 }
